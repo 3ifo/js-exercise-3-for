060c1f08fbbf02de6c1dbb6fb21786b0f54227d2
leonardo <leonardo_trifiletti@yahoo.it>
2023-10-25 17:47:47 +0200

    3 esercizio fatto

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 /* 2. Fai inserire un numero, che chiameremo N, all’utente.
 Genera N array, ognuno formato da 10 numeri casuali tra 1 e 100.*/
 
-let n = parseInt(prompt("Inserisci un numero"));
+/* let n = parseInt(prompt("Inserisci un numero"));
 if (isNaN(n)) {
   alert("inserisci un numero valido");
 } else {
@@ -15,4 +15,23 @@ if (isNaN(n)) {
     arrayParziale.push(numeroCasuale);
     console.log(numeroCasuale);
   }
+} */
+
+/* 3. In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa. */
+
+let arrayInvitati = [
+  "Marco",
+  "Filippo",
+  "Jacopo",
+  "Leonardo",
+  "Saverio",
+  "Carmine",
+];
+
+let invitati = prompt("Come ti chiami?");
+
+if (arrayInvitati.includes(invitati)) {
+  alert("Puoi accedere");
+} else {
+  alert("non puoi entrare mi dispiace");
 }
